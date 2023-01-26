8cf8c235c57e59d0964b2980973efd55e356ee74
clbrem <cbremer4@gmail.com>
2023-01-25 22:24:00 -0600

    hello world

---
+++ FiascoBot/Program.fs
@@ -0,0 +1,39 @@
+﻿open System
+open System.Threading.Tasks
+open Discord
+open Discord.WebSocket
+open Microsoft.Extensions.Configuration
+[<CLIMutable>]
+type Secrets =
+    { token: string }
+let config() =
+    ConfigurationBuilder()
+        .AddUserSecrets<Secrets>(false)
+        .Build()
+let log (msg: LogMessage) =
+    printfn $"{msg.ToString()}"
+    Task.CompletedTask        
+let program _ =
+    let config = config()
+    let token = config["token"]
+    
+    task {
+        use client = new DiscordSocketClient()
+        do client.add_Log(log)
+        do! client.LoginAsync(TokenType.Bot, token)
+        do! client.StartAsync()
+        do! Task.Delay(-1)   
+    }
+    
+    
+
+
+
+
+[<EntryPoint>]
+let main args =
+    (program args).GetAwaiter().GetResult()
+    0
+    
+    
+    
